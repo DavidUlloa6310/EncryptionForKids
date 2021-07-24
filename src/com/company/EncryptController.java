b546d3b9e912b7ccf692917ff2343e0a925d92f6
David Ulloa <dulloa6310@gmail.com>
2021-07-24 12:11:57 -0400

    Added check box to on Encrypt.fxml to show encryption values of each color at each square. Also, created PaintingArray.java to organize the array for painting. Will have encryption method to mame code simpler.

--- src/com/company/EncryptController.java
+++ src/com/company/EncryptController.java
@@ -3,12 +3,14 @@ package com.company;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 public class EncryptController {
 
@@ -30,20 +32,26 @@ public class EncryptController {
     @FXML
     private RadioButton whiteButton;
 
+    @FXML
+    private CheckBox showKeysButton;
+
     final ToggleGroup group = new ToggleGroup();
 
     private GraphicsContext gc;
 
+    private PaintingArray paintingArray;
 
     @FXML
     private void initialize() {
+        paintingArray = new PaintingArray();
+
         gc = canvas.getGraphicsContext2D();
 
-        canvas.setHeight(Main.getHEIGHT_TILES() * 25);
-        canvas.setWidth(Main.getWIDTH_TILES() * 25);
+        canvas.setHeight(Main.getHEIGHT_TILES() * Main.getTileSize());
+        canvas.setWidth(Main.getWIDTH_TILES() * Main.getTileSize());
 
         gc.setFill(Color.WHITE);
-        gc.fillRect(0, 0, Main.getWIDTH_TILES() * 25, Main.getHEIGHT_TILES() * 25);
+        gc.fillRect(0, 0, Main.getWIDTH_TILES() * Main.getTileSize(), Main.getHEIGHT_TILES() * Main.getTileSize());
 
         gc.setFill(Color.RED);
 
@@ -70,14 +78,87 @@ public class EncryptController {
         if (xBox > Main.getWIDTH_TILES() - 1 || xBox < 0 || yBox < 0 || yBox > Main.getHEIGHT_TILES() - 1)
             return;
 
-        int xPixel = xBox * 25;
-        int yPixel = yBox * 25;
+        int xPixel = xBox * Main.getTileSize();
+        int yPixel = yBox * Main.getTileSize();
 
         gc.fillRect(xPixel, yPixel, Main.getTileSize(), Main.getTileSize());
+
+        if (Color.RED.equals(gc.getFill())) {
+            paintingArray.fillSquare(yBox, xBox, "001");
+        } else if (Color.WHITE.equals(gc.getFill())) {
+            paintingArray.fillSquare(yBox, xBox, "000");
+        } else if (Color.GREEN.equals(gc.getFill())) {
+            paintingArray.fillSquare(yBox, xBox, "011");
+        } else if (Color.BLUE.equals(gc.getFill())) {
+            paintingArray.fillSquare(yBox, xBox, "010");
+        } else if (Color.YELLOW.equals(gc.getFill())) {
+            paintingArray.fillSquare(yBox, xBox, "100");
+        }
+
+        Paint prevColor = gc.getFill();
+
+        gc.setFill(Color.BLACK);
+
+        if (showKeysButton.isSelected()) {
+            gc.fillText(paintingArray.getArray()[yBox][xBox], xBox * Main.getTileSize(), yBox * Main.getTileSize() + Main.getTileSize(), Main.getTileSize());
+        }
+
+        gc.setFill(prevColor);
     }
 
     public void onMouseDragged(MouseEvent e) {
         onMouseLeftClick(e.getX(), e.getY());
+    }
+
+    public void showKeys() {
+
+        if (showKeysButton.isSelected()) {
+            Paint prevColor = gc.getFill();
+
+            gc.setFill(Color.BLACK);
+
+            for (int r = 0; r < Main.getHEIGHT_TILES(); r++) {
+                for (int c = 0; c < Main.getWIDTH_TILES(); c++) {
+                    gc.fillText(paintingArray.getArray()[r][c], c * Main.getTileSize(), r * Main.getTileSize() + Main.getTileSize(), Main.getTileSize());
+                }
+            }
+
+            gc.setFill(prevColor);
+        } else {
+            paintCanvas();
+        }
+    }
+
+    public void paintCanvas() {
+
+        Paint prevPaint = gc.getFill();
+
+        for (int r = 0; r < Main.getHEIGHT_TILES(); r++) {
+            for (int c = 0; c < Main.getWIDTH_TILES(); c++) {
+                String square = paintingArray.getArray()[r][c];
+                switch (square) {
+                    case "000":
+                        gc.setFill(Color.WHITE);
+                        break;
+                    case "001":
+                        gc.setFill(Color.RED);
+                        break;
+                    case "010":
+                        gc.setFill(Color.BLUE);
+                        break;
+                    case "011":
+                        gc.setFill(Color.GREEN);
+                        break;
+                    case "100":
+                        gc.setFill(Color.YELLOW);
+                        break;
+                }
+
+                gc.fillRect(c * Main.getTileSize(), r * Main.getTileSize(), Main.getTileSize(), Main.getTileSize());
+            }
+        }
+
+        gc.setFill(prevPaint);
     }
 
     public void redClick() {
---
+++ src/com/company/PaintingArray.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import com.company.Main;
+
+public class PaintingArray {
+    private String[][] array;
+
+    public PaintingArray() {
+        resetArray();
+    }
+
+    public void resetArray() {
+
+        array = new String[Main.getHEIGHT_TILES()][Main.getWIDTH_TILES()];
+
+        for (int r = 0; r < Main.getHEIGHT_TILES(); r++) {
+            for (int c = 0; c < Main.getWIDTH_TILES(); c++) {
+                array[r][c] = "000";
+            }
+        }
+    }
+
+    public void fillSquare(int r, int c, String value) {
+        array[r][c] = value;
+    }
+
+    public String encrypt() {
+        return "";
+    }
+
+    public String[][] getArray() {
+        return array;
+    }
+
+    public void printArray() {
+        for (int r = 0; r < array.length; r++) {
+            for (int c = 0; c < array[0].length; c++) {
+                System.out.print(array[r][c]);
+                System.out.print(" ");
+            }
+            System.out.println("\n");
+        }
+    }
+}
